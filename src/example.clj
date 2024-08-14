b74f201ff719c7d471a3d0494e58c75158e63196
Jukka Villstedt <jukka.villstedt@iki.fi>
2024-08-14 14:17:49 +0300

    update example

--- src/example.clj
+++ src/example.clj
@@ -1,48 +1,100 @@
 (ns example
-  (:require [keepa.core :as core]
-            [keepa.cryptography :as cryptography]))
+  (:require
+   [clojure.edn :as edn]
+   [clojure.java.io :as io]
+   [keepa.core :as core]
+   [keepa.cryptography :as cryptography]
+   [keepa.shell :as shell]
+   [keepa.store :as store]
+   [keepa.qr :as qr]))
 
 (def master-path "temp/example-master")
 (def hash-file-path (core/path master-path "master-hash"))
-(def public-key-paths [(core/path master-path "example-keep.public")])
 (def store-path (core/path master-path "store"))
 
-(comment
-  (core/make-keep "example-keep" "temp/example-keep")
+(defn encryption-keys [password]
+  [{:key password
+    :name "string"}
+   {:key (core/load-key "paper-keep-1.public")
+    :name "paper-1"}
+   {:key (core/load-key "paper-keep-2.public")
+    :name "paper-2"}
+   {:key (core/load-key "master-keep.public")
+    :name "master"}])
 
-  (core/make-paper-keep "example-paper-keep"
-                        "temp/example-paper-keep"
-                        {:url "http://example.com"
-                         :username "foo"
-                         :password "bar"})
+(defn sync-remote-directory [local-directory username host remote-directory]
+  (shell/run-command "rsync"  "-e" "ssh -i ~/.ssh/id_ecdsa" "-a" (str local-directory "/")  (str username "@" host ":" remote-directory)))
+
+(defn private-keys []
+  {"string" (core/ask-and-check-password hash-file-path)
+   "master" (core/load-key (core/path master-path "master-keep/master-keep.secret"))
+   ;; "paper-1" (core/load-paper-keep-key-from-image-file "paper-1.jpg")
+   ;; "paper-2" (core/load-paper-keep-key-from-image-file "paper-2.jpg")
+   })
+
+(defn spit-encrypted [secret-name contents]
+  (core/spit-encrypted store-path
+                       secret-name
+                       (encryption-keys (core/ask-and-check-password hash-file-path))
+                       contents))
+
+(defn decrypt [file-name]
+  (core/decrypt (private-keys)
+                (edn/read-string (slurp file-name))))
+
+
+
+(comment
+
+  ;; creating keeps
+
+  (store/set-password "example.com"
+                      "keep1"
+                      "foo")
+
+  (core/make-keep "keep1" "/Volumes/KEEP1")
+
+  (core/make-keep "master-keep" (core/path master-path "master-keep"))
+
+  (core/make-paper-keep "paper-keep-3"
+                        "local/paper-keep-3"
+                        [{:url "example.com/public/secret"
+                          :username "user"
+                          :password "pwd"}])
+
+  (spit (core/path master-path "paper-keep-1.public")
+        (cryptography/encode (cryptography/public-key (core/load-paper-keep-key-from-image-file "paper.jpg"))))
 
   (core/create-password-hash-file hash-file-path)
 
-  (core/spit-file-and-encrypt-with-password-and-key-combination "my secret message"
-                                                                "my-secret-message"
-                                                                store-path
-                                                                hash-file-path
-                                                                public-key-paths)
+  (qr/text-from-qr-code-image-file "paper-1.jpeg")
 
-  (core/backup-file-and-encrypt-with-password-and-key-combination "/Users/example-user/.ssh/id_rsa"
-                                                                  store-path
-                                                                  hash-file-path
-                                                                  public-key-paths)
 
-  (core/edit-file-with-password-and-keys (core/path master-path "my-secret-editable-file")
-                                         hash-file-path
-                                         public-key-paths
-                                         store-path)
 
-  (core/sync-remote-directory store-path
-                              "example-user"
-                              "example.com"
-                              "example-keep")
 
-  (cryptography/decrypt (slurp (core/path store-path "my-secret-message-2018-11-22-20-05-34_example-keep") )
-                        (core/load-key "/Volumes/example-keep/example-keep.secret"))
+  ;; encryption
 
-  (cryptography/decrypt (slurp (core/path store-path "my-secret-message-2018-11-22-20-05-34_example-keep") )
-                        (core/ask-and-check-password hash-file-path))
+  (do (def keepass-export-file "keepass.csv")
+
+      (spit-encrypted "data"
+                      (slurp keepass-export-file))
+
+      (io/delete-file keepass-export-file))
+
+  (spit-encrypted "some-file-encrypted" (slurp "some-file"))
+
+
+  ;; remote storage sync
+
+  (sync-remote-directory store-path
+                         "user"
+                         "example.com"
+                         "/var/www/html/public/")
+
+
+  ;; decrypt
+
+
+  (decrypt "encrypted-file")
 
   )
